7ea6a2d1d805be9969c193841f22a88e25a7349f
Jennifer <jen.yiu.90@gmail.com>
2018-11-20 14:11:08 -0800

    change variables; codealong in content.php

--- index.php
+++ index.php
@@ -10,28 +10,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+		 if ( have_posts() ) : 
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			while( have_posts() ): the_post();
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+				get_template_part( 'template-parts/content' );
 
-			<?php endwhile; ?>
+			endwhile;
+		
+		else:
 
-			<?php the_posts_navigation(); ?>
+			get_template_part( 'template-parts/content', 'none' );
 
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
+		 endif; 
+		 
+		 ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -5,14 +5,36 @@
  * @package QOD_Starter_Theme
  */
 
+// method to retrieving meta data in WP
+$source = get_post_meta( get_the_ID(), '_qod_quote_source', true );
+$source_url = get_post_meta( get_the_ID(), '_qod_quote_source_url', true );
+	// var_dump($source_url);
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
+
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
+
+	<div class="entry-meta">
+		<?php the_title( '<h2 class="entry-title">&mdash; ', '</h2>' ); ?>
+
+		<?php if( $source && $source_url ): ?>
+
+			<span class="source">, <a href="<?php echo $source_url; ?>"><?= $source; ?></a></span>
+		
+		<?php elseif( $source ): ?>
+
+			<span class="source">, <?= $source; ?></span>
+
+		<?php else: ?>
+
+			<span class="source"></span>
+			
+		<?php endif; ?>
+
+	</div><!-- .entry-meta -->
+
 </article><!-- #post-## -->
