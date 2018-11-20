863706c39dc8e2c116690ecbd37ad6935df4764c
Jennifer <jen.yiu.90@gmail.com>
2018-11-20 11:49:21 -0800

    add TODO tasks in functions.php; add fonts

--- functions.php
+++ functions.php
@@ -60,6 +60,16 @@ function qod_scripts() {
 
 	wp_enqueue_script( 'qod-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'qod-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
+
+
+// TODO add a custom javascript file
+
+
+// TODO try to add a localized script
+
+
+
+
 }
 add_action( 'wp_enqueue_scripts', 'qod_scripts' );
 
