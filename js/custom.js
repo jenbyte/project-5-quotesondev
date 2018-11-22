e98b2457ff5af91690f962ff7b26e2067df63858
Jennifer <jen.yiu.90@gmail.com>
2018-11-22 07:06:41 -0800

    update JS, style form and page-template

--- js/custom.js
+++ js/custom.js
@@ -12,12 +12,18 @@
         method: 'GET',
         url:
           qod_vars.rest_url +
-          'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1' //can add filter with query string (?)
+          'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        //can add filter with query string (?)
+        data: {
+          action: 'qod_scripts',
+          security: qod_vars.comment_nonce,
+          the_post_id: qod_vars.post_id
+        }
       })
         .done(function(data) {
           // append content to DOM e.g. replace the quote content with rest api content
-          $('.entry-content').append();
-          console.log(data);
+          $('.post').append(qod_vars.data);
+          console.log(qod_vars.rest_url.data);
         })
         .fail(function(err) {
           // Append an error message or alert
